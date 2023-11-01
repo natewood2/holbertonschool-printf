d76bbc15a318367b4345d3bf723649897eddd33c
Nathan Wood <natew.@Nathans-MacBook-Air.local>
2023-11-01 15:14:58 -0500

    Updated main.h file with needed libraries.

--- main.h
+++ main.h
@@ -5,12 +5,21 @@
 #include <stdbool.h>
 #include <stdarg.h>
 #include <string.h>
-int _putchar(char c);
 
+int _putchar(char c);
+/**
+ * struct my_printf - struct op
+ *
+ * @op: op
+ * my_printf - struct for the remake of printf function
+ * @f: the function associated
+ */
 typedef struct my_printf
 {
-        char *op;
-        void(*f)(void*);
+	char *op;
+	void (*f)(void *);
+
 } my_printf;
 
 #endif
+
